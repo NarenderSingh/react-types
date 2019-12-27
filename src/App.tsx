e83ac51260c0c95822cb6974e53b4990eb7ca4d5
NarenderSingh <naren.delhi13@gmail.com>
2019-12-28 01:21:49 +0530

    tuple / enum / any / void of typescript

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,32 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    let firstValue: string = "Many";
-    let values: number[] = [1, 2, 3];
+    // tuple
+    let Tuple: [string, number, string] = ["React", 1, "using TypeScript"];
+    // enum
+    enum Codes {
+      first = 1,
+      second = 2
+    }
+    // any
+    let FirstName: any = "React";
+    // void
+    const warning = (): void => {
+      console.log("warning");
+    };
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            The value {firstValue} is of {typeof firstValue} type ! <br />
-            The value {values} is of {typeof values} type !
+            The value {Tuple[0]} is of {typeof Tuple[0]} type ! <br />
+          </p>
+          <p>
+            The value {Codes.first} is of {typeof Codes.first} type ! <br />
+          </p>
+          <p>
+            <button onClick={warning}>Warning</button>
           </p>
         </header>
       </div>
