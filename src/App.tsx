42fab85366b1fd7753399f513cef2b48654f7d61
NarenderSingh <naren.delhi13@gmail.com>
2019-12-28 02:14:01 +0530

    typescript for interface, class and state

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import Message from "./component/Message";
 
-class App extends Component {
+const initState = {
+  name: "React using Typescript",
+  message: "Hello from React"
+};
+
+type State = Readonly<typeof initState>;
+
+class App extends Component<any, State> {
+  readonly state: State = initState;
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Message />
+          <Message name={this.state.name} message={this.state.message} />
         </header>
       </div>
     );
